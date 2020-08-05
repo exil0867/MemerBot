a77f43695293ca0c045f4079a6df181b359820e0
ExillustX <mrexillustx@gmail.com>
2020-08-05 23:53:46 +0100

    add invite command to the help menu

--- commands/help.js
+++ commands/help.js
@@ -10,6 +10,9 @@ exports.run = (bot, msg, args) => {
         }, {
           'name': process.env.PREFIX + 'meme',
           'value': `Generate a meme (Supports normal images and GIFs). \nUsage: \`${process.env.PREFIX}meme\ <link> top text | bottom text\`\nUsage (with an image attachement): \`${process.env.PREFIX}meme\ top text | bottom text\``
+        }, {
+          'name': process.env.PREFIX + 'invite',
+          'value': `Invite the bot to your server.`
         }
       ],
       'color': 3264944,
