e0f471b2164f6e6b2568579161dabed22724c4b9
ExillustX <mrexillustx@gmail.com>
2020-08-07 10:44:02 +0100

    unleashing the recursive directory maker

--- utils/dl-file.js
+++ utils/dl-file.js
@@ -1,13 +1,16 @@
+const fsp = require('fs').promises;
 const fs = require('fs');
 
 const downloadFile = (buffer, dirPath, filePath) => {
   return new Promise((resolve, reject) => {
-    if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
+    fs.promises.mkdir(dirPath, { recursive: true }).then(() => {
       fs.writeFile(filePath, buffer, 'binary', err => {
         if (err) return reject(err);
         resolve();
       });
-
+    }).catch(err => {
+      return reject(err);
+    });
   });
 }
 
