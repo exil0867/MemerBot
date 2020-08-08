8af63f4698008a7d9034db2d021bd92edafd9c1b
ExillustX <mrexillustx@gmail.com>
2020-08-08 11:11:39 +0100

    aaaaaaaa

--- utils/hostname-check.js
+++ utils/hostname-check.js
@@ -1,7 +1,7 @@
 const url = require('url');
 
-const hostnameCheck = (url, hostname) => {
-  const _hostname = url.parse(tenorUrl).hostname;
+const hostnameCheck = (_url, hostname) => {
+  const _hostname = url.parse(_url).hostname;
   if (_hostname !== hostname) return false;
   return true;
 }
