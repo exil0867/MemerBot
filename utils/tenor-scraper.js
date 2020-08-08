8fbea30e7e48e45f320224e611c293ba45c68955
ExillustX <mrexillustx@gmail.com>
2020-08-08 11:03:41 +0100

    small fixes xoxo

--- utils/tenor-scraper.js
+++ utils/tenor-scraper.js
@@ -3,11 +3,11 @@ const cheerio = require('cheerio');
 const isImageUrl = require('is-image-url');
 const url = require('url');
 
-const tenorScraper = url => {
+const tenorScraper = tenorUrl => {
   return new Promise( async (resolve, reject) => {
-    const hostName = url.parse(url).hostName;
+    const hostName = url.parse(tenorUrl).hostname;
     if (hostName !== 'tenor.com') reject(new Error('Hostname is not a tenor.com'))
-    const response = await fetch(url).catch(err => reject(err));
+    const response = await fetch(tenorUrl).catch(err => reject(err));
     const html = await response.text().catch(err => reject(err));
     const $ = cheerio.load(html);
     const directGifLink = $('[property="og:url"]')['0'].attribs.content;
